7f02e537dc9307acf7b7760a0bbdea8012f0c486
tengmo3210 <tengmo02@gmail.com>
2017-09-10 00:15:25 +0700

    use crispy form

--- trtTen/src/contact/views.py
+++ trtTen/src/contact/views.py
@@ -3,9 +3,13 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 from .forms import contactForm
+
 # Create your views here.
 def contact(request):
+    title = "Contact"
     form = contactForm(request.POST or None)
+    context = {'title': title, 'form':form,}
+    confirm_massage =None
 
     if form.is_valid():
     #    print form.cleaned_data['email']
@@ -16,6 +20,10 @@ def contact(request):
         emailForm = form.cleaned_data['email']
         emailTo = [settings.EMAIL_HOST_USER]
         send_mail( subject , message, emailForm, emailTo, fail_silently= True)
-    context = locals()
+        title = "Thaanks!"
+        confirm_massage = "Thaanks for the Massage. We will get right back to you."
+        form = None
+    #context = locals()
+    context = {'title': title,'form': form ,'confirm_massage' : confirm_massage, }
     template = 'contact.html'
     return render(request,template,context)
