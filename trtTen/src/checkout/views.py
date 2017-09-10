cce92d03d56dcd02e3ab67496864f11abdb95322
tengmo3210 <tengmo02@gmail.com>
2017-09-10 16:44:15 +0700

    crate stripe id with each user

--- trtTen/src/checkout/views.py
+++ trtTen/src/checkout/views.py
@@ -9,14 +9,17 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 @login_required(login_url='/accounts/login/')
 def checkout(request):
     publishKey = settings.STRIPE_PUBLISHABLE_KEY
+    customer_id = request.user.userstripe.stripe_id
 
     if request.method == "POST":
         token = request.POST['stripeToken']
         try:
+            customer = stripe.Customer.retrieve(customer_id)
+            customer.sources.create(source= token )
             charge = stripe.Charge.create(
                 amount=1000, ##Amount in cent
                 currency="usd",
-                source=token,
+                customer = customer,
                 description="Example charge"
             )
         except stripe.error.CardError as e:
